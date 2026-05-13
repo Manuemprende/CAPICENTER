412b7b2ce8fec953993c8f94bd6090a338a86c35
Manu.04 <man.torresp35@gmail.com>
2026-05-12 21:15:17 -0400

    Update check_sheets_content with safe print

--- check_sheets_content.ts
+++ check_sheets_content.ts
@@ -5,9 +5,10 @@ async function check() {
   const url = `https://docs.google.com/spreadsheets/d/${source.id}/export?format=csv&gid=${source.gid}`;
   const res = await axios.get(url);
   const lines = String(res.data).split(/\r?\n/);
-  console.log('--- SHEET DATA (First 10 lines) ---');
-  for (let i = 0; i < Math.min(10, lines.length); i++) {
-    console.log(lines[i]);
+  console.log('--- SHEET DATA ---');
+  for (let i = 0; i < Math.min(20, lines.length); i++) {
+    // Replace non-ascii characters to avoid encoding issues in windows print
+    console.log(lines[i].replace(/[^\x00-\x7F]/g, ""));
   }
 }
 
